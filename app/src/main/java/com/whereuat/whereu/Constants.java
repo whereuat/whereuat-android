b4dac10f68848e05a4074f403bb31d0f72bcd97e
Julius Alexander IV <alexaj5rpi@gmail.com>
2016-04-04 18:40:37 -0400

    Implements the login workflow.
    Implements the login workflow.

---
+++ app/src/main/java/com/whereuat/whereu/Constants.java
@@ -0,0 +1,17 @@
+package com.whereuat.whereu;
+
+/**
+ * Created by julius on 4/3/16.
+ */
+public class Constants {
+    public static final String WHEREUAT_URL = "http://whereuat.xyz/";
+    public static final String ACCOUNT_REQUEST_ROUTE = "account/request";
+    public static final String ACCOUNT_NEW_ROUTE = "account/new";
+
+    public static final String RESPONSE_CODE_EXTRA = "RESPONSE_CODE";
+
+    public static final String ACCOUNT_REQUEST_BROADCAST =
+            "com.whereuat.whereu.ACCOUNT_REQUEST_BROADCAST";
+    public static final String ACCOUNT_NEW_BROADCAST =
+            "com.whereuat.whereu.ACCOUNT_NEW_BROADCAST";
+}
--- app/src/main/java/com/whereuat/whereu/LoginActivity.java
+++ app/src/main/java/com/whereuat/whereu/LoginActivity.java
@@ -1,92 +1,165 @@
 package com.whereuat.whereu;
 
-import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class LoginActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
-    Button b;
-    EditText ed;
+    private EditText mPhoneEdit;
+    private EditText mVerifyCode;
+    private PreferenceController mPrefs;
+    private RelativeLayout mAccountRequestSection;
+    private RelativeLayout mAccountCreateSection;
+    private HttpRequestHandler mHttpReqHandler;
+    private AccountNewBroadcastReceiver mAccountNewReceiver;
+    private AccountRequestBroadcastReceiver mAccountRequestReceiver;
+    private IntentFilter mAccountNewFilter;
+    private IntentFilter mAccountRequestFilter;
 
-    TextView tx1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mPrefs = new PreferenceController(this);
+        mPhoneEdit = (EditText) findViewById(R.id.phone_number_input);
+        mVerifyCode = (EditText) findViewById(R.id.verification_code_input);
+        mAccountRequestSection = (RelativeLayout) findViewById(R.id.account_request_section);
+        mAccountCreateSection = (RelativeLayout) findViewById(R.id.account_create_section);
+        mHttpReqHandler = new HttpRequestHandler(this);
 
-        b=(Button)findViewById(R.id.button);
-        ed=(EditText)findViewById(R.id.editText);
+        mAccountNewFilter = new IntentFilter(Constants.ACCOUNT_NEW_BROADCAST);
+        mAccountNewReceiver = new AccountNewBroadcastReceiver();
+        registerReceiver(mAccountNewReceiver, mAccountNewFilter);
 
+        mAccountRequestFilter = new IntentFilter(Constants.ACCOUNT_REQUEST_BROADCAST);
+        mAccountRequestReceiver = new AccountRequestBroadcastReceiver();
+        registerReceiver(mAccountRequestReceiver, mAccountRequestFilter);
 
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-              login();
-            }
-        });
-    }
-
-    public void login(){
-        Log.d(TAG, "Login");
-
-
-        b.setEnabled(false);
-
-//        final ProgressDialog progressDialog = new ProgressDialog(MainActivity.this,
-//                R.style.AppTheme_Dark_Dialog);
-        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Creating Profile...");
-        progressDialog.show();
-
-        String phone_num = ed.getText().toString();
-
-        // TODO: Implement your own authentication logic here.
-
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onLoginSuccess or onLoginFailed
-//                        onLoginSuccess();
-
-                        // onLoginFailed();
-                        progressDialog.dismiss();
-                    }
-                }, 3000);
+        if (mPrefs.isWaitingForVerify())
+            showCreateHideRequest();
+        else
+            showRequestHideCreate();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onPause() {
+        try {
+            unregisterReceiver(mAccountNewReceiver);
+        } catch (IllegalArgumentException e) {}
+
+        try {
+            unregisterReceiver(mAccountRequestReceiver);
+        } catch (IllegalArgumentException e) {}
+        super.onPause();
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+    public void onResume() {
+        try {
+            registerReceiver(mAccountNewReceiver, mAccountNewFilter);
+        } catch (IllegalArgumentException e) {}
 
-        int id = item.getItemId();
+        try {
+            registerReceiver(mAccountRequestReceiver, mAccountRequestFilter);
+        } catch (IllegalArgumentException e) {}
+        super.onResume();
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void showCreateHideRequest() {
+        mAccountCreateSection.setVisibility(View.VISIBLE);
+        mAccountRequestSection.setVisibility(View.GONE);
+    }
+
+    /*
+        If the phone number is valid, sends an account request to the server and changes the UI to
+        the verification view.
+     */
+    public void requestAccount(View v) {
+        String phone_number = mPhoneEdit.getText().toString();
+        if (isValidPhoneForm(phone_number)) {
+            mHttpReqHandler.postAccountRequest(phone_number);
+            mPrefs.setClientPhoneNumberPref(phone_number);
+        } else {
+            // TODO: There's a better way to handle this because the user shouldn't really have to
+            // put the string in this form.
+            String text = "The number should be in the form '+1xxxxxxxxxx'.";
+            Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
         }
-        return super.onOptionsItemSelected(item);
+    }
+
+    private void showRequestHideCreate() {
+        mAccountCreateSection.setVisibility(View.GONE);
+        mAccountRequestSection.setVisibility(View.VISIBLE);
+    }
+
+    private String generateGcmToken() {
+        return "la-dee-da-I'm a token";
+    }
+
+    public void createNewAccount(View v) {
+        mHttpReqHandler.postAccountNew(mPrefs.getClientPhoneNumber(), generateGcmToken(),
+                mVerifyCode.getText().toString());
+    }
+
+    /*
+     * Checks that the phone number is in the valid form.
+     */
+    private boolean isValidPhoneForm(String phone_number) {
+        String pattern = "\\+1\\d{10}";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(phone_number);
+        return m.find();
+    }
+
+    /*
+        A receiver for handling account/new POST responses. This receiver will alter the
+        SharedPreferences for waiting to verify and has account and will launch the contact grid
+        activity if the response is OK.
+     */
+    private class AccountNewBroadcastReceiver extends BroadcastReceiver {
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getIntExtra(Constants.RESPONSE_CODE_EXTRA, 400) == 200) {
+                mPrefs.setHasAccountPref(true);
+                mPrefs.setWaitingForVerifyPref(false);
+                LoginActivity.this.startActivity(
+                        new Intent(LoginActivity.this, MainActivity.class));
+                LoginActivity.this.finish();
+            } else {
+                String text = "Error making your account :(";
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+                showRequestHideCreate();
+            }
+        }
+    }
+
+    /*
+        A receiver for handling account/request POST responses. This receiver will show the
+        UI for entering the verification code and set the SharedPreference for waiting to verify.
+     */
+    private class AccountRequestBroadcastReceiver extends BroadcastReceiver {
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getIntExtra(Constants.RESPONSE_CODE_EXTRA, 400) == 200) {
+                mPrefs.setWaitingForVerifyPref(true);
+                showCreateHideRequest();
+            } else {
+                String text = "Error making your account request :(";
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+                showRequestHideCreate();
+                mPrefs.setWaitingForVerifyPref(false);
+            }
+        }
     }
 }
-
