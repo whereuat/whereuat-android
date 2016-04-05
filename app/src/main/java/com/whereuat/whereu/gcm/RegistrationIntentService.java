c6d43767324340758cb4e46cd6b3deb604edbd4b
Peter Kang <peterjkang@gmail.com>
2016-04-04 21:26:20 -0400

    Get rid of GcmPreferences class

--- app/src/main/java/com/whereuat/whereu/gcm/RegistrationIntentService.java
+++ app/src/main/java/com/whereuat/whereu/gcm/RegistrationIntentService.java
@@ -18,8 +18,6 @@ package com.whereuat.whereu.gcm;
 
 import android.app.IntentService;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
@@ -38,11 +36,9 @@ public class RegistrationIntentService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         String token = "";
         try {
-            // [START register_for_gcm]
             // Initially this call goes out to the network to retrieve the token, subsequent calls
             // are local.
             // R.string.gcm_defaultSenderId (the Sender ID) is typically derived from google-services.json.
@@ -52,17 +48,8 @@ public class RegistrationIntentService extends IntentService {
             token = instanceID.getToken(getString(R.string.gcm_defaultSenderId),
                     GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
             // [END get_token]
-
-            // You should store a boolean that indicates whether the generated token has been
-            // sent to your server. If the boolean is false, send the token to your server,
-            // otherwise your server should have already received the token.
-            sharedPreferences.edit().putBoolean(GcmPreferences.SENT_TOKEN_TO_SERVER, true).apply();
-            // [END register_for_gcm]
         } catch (Exception e) {
             Log.d(TAG, "Failed to complete token refresh", e);
-            // If an exception happens while fetching the new token or updating our registration data
-            // on a third-party server, this ensures that we'll attempt the update at a later time.
-            sharedPreferences.edit().putBoolean(GcmPreferences.SENT_TOKEN_TO_SERVER, false).apply();
         }
         // Broadcast the token to the login activity.
         Intent registrationComplete = new Intent(Constants.TOKEN_BROADCAST);
