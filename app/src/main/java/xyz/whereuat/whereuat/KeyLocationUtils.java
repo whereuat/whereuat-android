3b5be09dbeaea1ebefd17598c9a4c2a6ad24e8d4
Peter Kang <peterjkang@gmail.com>
2016-04-16 23:23:50 -0400

    Add nearest loc utility function

--- app/src/main/java/xyz/whereuat/whereuat/KeyLocationUtils.java
+++ app/src/main/java/xyz/whereuat/whereuat/KeyLocationUtils.java
@@ -2,6 +2,7 @@ package xyz.whereuat.whereuat;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.location.Location;
 
 import xyz.whereuat.whereuat.db.command.InsertCommand;
@@ -19,6 +20,31 @@ public class KeyLocationUtils {
 
     public static boolean locIsValid(Location loc) {
         return loc != null;
+    }
+
+    public static String getNearestLoc(Cursor c, Location origin) throws IllegalArgumentException {
+        String nearest = null;
+        double nearest_dist = Double.MAX_VALUE;
+
+        c.moveToFirst();
+        while(c.moveToNext()) {
+            double longitude = c.getDouble(c.getColumnIndexOrThrow(
+                    KeyLocationEntry.COLUMN_LONGITUDE));
+            double latitude = c.getDouble(c.getColumnIndexOrThrow(
+                    KeyLocationEntry.COLUMN_LATITUDE));
+
+            Location loc = new Location("");
+            loc.setLongitude(longitude);
+            loc.setLatitude(latitude);
+
+            double dist = loc.distanceTo(origin);
+            if (dist < nearest_dist) {
+                nearest = c.getString(c.getColumnIndexOrThrow(KeyLocationEntry.COLUMN_NAME));
+                nearest_dist = dist;
+            }
+        }
+
+        return nearest;
     }
 
     public static InsertCommand buildInsertCommand(Context context, String name, double latitude,
